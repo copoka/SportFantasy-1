72e47eb4b001f479c2bdf0b179839b10ee19b594
shade88 <shade88@yandex.ru>
2014-10-27 15:31:00 +0600

    changed migrations

--- db/migrate/20141027051824_create_user_teams.rb
+++ db/migrate/20141027051824_create_user_teams.rb
@@ -1,7 +1,7 @@
 class CreateUserTeams < ActiveRecord::Migration
   def change
     create_table :user_teams do |t|
-      t.belongs_to :user
+      t.belongs_to :user, index: true
       t.string :name
       t.integer :score
 
--- db/migrate/20141027063449_create_players.rb
+++ db/migrate/20141027063449_create_players.rb
@@ -2,8 +2,8 @@ class CreatePlayers < ActiveRecord::Migration
   def change
     create_table :players do |t|
       t.string :name
-      t.belongs_to :real_team
-      t.belongs_to :amplua
+      t.belongs_to :real_team, index: true
+      t.belongs_to :amplua, index: true
       t.float :price
       t.integer :score
 
