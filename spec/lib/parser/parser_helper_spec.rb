8750d65f7a5325e38050708aa91eb34f76900f70
Peter Chudinov <haulhd@gmail.com>
2014-10-29 19:29:28 +0200

    Тесты для calculate_price

--- spec/lib/parser/parser_helper_spec.rb
+++ spec/lib/parser/parser_helper_spec.rb
@@ -6,5 +6,11 @@ RSpec.describe ParserHelper , :type => :helper do
     it 'calculate price correctly' do
       expect(helper.calculate_price [3, 6, 9]).to eq 10_300_000
     end
+     it 'calculate price correctly with float values' do
+      expect(helper.calculate_price [4, 5, 6]).to eq 10_250_000
+    end
+     it 'calculate price correctly with big float values' do
+      expect(helper.calculate_price [228, 147, 97]).to eq 8_866_666
+    end
   end
 end
