e8856abd2f24171c21821548631ca7695cec6696
Peter Chudinov <peterchudinov@icloud.com>
2014-11-01 23:43:22 +0200

    Создал контроллер для поиска.
    Да, он пустой пока.

--- lib/parser/parser_helper.rb
+++ lib/parser/parser_helper.rb
@@ -1,7 +1,9 @@
 module ParserHelper
   # массив очков из n(3-4) прошлых сезонов
   def calculate_price(past_score)
+
     me = past_score.inject(0) { |sym, n| sym + n/past_score.size.to_f }
     10_000_000 + (me * 50_000).to_i
+  
   end
 end
