5d451c756ca69fdf7ff1c435e660f7398cbdd486
PeterChudinov <haulhd@gmail.com>
2014-11-02 04:39:37 +0200

    Подключаем Sphinx
    Черновик для индексации. НЕ РАБОТАЕТ.

---
+++ app/indices/player_index.rb
@@ -0,0 +1,16 @@
+###
+# Это - черновик. Надо переписать БД, чтобы все это выглядело не так стремно.
+# Копаю Sphinx в любом случае.
+# И надо гемфайлы прописать.
+###
+
+ThinkingSphinx::Index.define :player, :with => :active_record do
+  # fields
+  indexes player.name, :as player,:sortable => true
+  indexes player.amplua, :as player, :sortable => true
+  indexes player.team, :as player, :sortable => true
+  indexes player.leagueID, :as player, :sortable => true
+
+  # attributes
+  has player.id, player.price
+end
