0a3b415e2262fac03b1515aac757d7e796fc28f1
PeterChudinov <haulhd@gmail.com>
2014-11-19 22:57:48 +0200

    CSV parser upd

--- lib/parser/init_csv_to_mysql.rb
+++ lib/parser/init_csv_to_mysql.rb
@@ -1,3 +1,5 @@
+#ЭТОТ ПАРСЕР ТОЛЬКО ДЛЯ ЕДИНИЧНОГО ИСПОЛЬЗОВАНИЯ - ЗАБИТЬ ТАБЛИЦЫ ИНФОЙ О ТЕКУЩЕМ СЕЗОНЕ СЕЙЧАС
+#ДЛЯ ПОСТОЯННОГО ОБНОВЛЕНИЯ ИНФЫ НАДО ПИСАТЬ ДРУГОЙ (НАПРИМЕР, ДЛЯ РАСЧЕТА for_playing_60_minutes_or_more)
 require 'csv'
 
 CSV.foreach(stats.csv, headers: true) do |row|
@@ -15,9 +17,36 @@ CSV.foreach(stats.csv, headers: true) do |row|
     prep_amplua_id = 1
   end
 
-  Player.create! ({
+  prep_for_playing_60_minutes_or_more = (row[2] / 60) - (row[2] mod 60)
+  prep_for_playing_up_to_60_minutes = row[2] mod 60
+
+  player = Player.new (
     name: row[0],
     apmlua_id: prep_amplua_id,
-    
-    })
+    real_team_id:
+  )
+  player.save
+
+  player.perfomance.new (
+    player_id: player.id,
+    amplua_id: player.amplua_id,
+    for_playing_up_to_60_minutes: prep_for_playing_up_to_60_minutes,
+    for_playing_60_minutes_or_more: prep_for_playing_60_minutes_or_more,
+    goals: row[4],
+    assists: row[5],
+    three_saves_gk: row[14],#row[14] / 3                                        #откуда, как? стоит просто поставить сейвы и уже каждые 3 высчитывать на логике
+    penalty_save_gk: row[11],
+    penalty_miss: ,
+    clean_sheet: row[6],
+    mvps: ,                                                                     #откуда, как? можно либо свой алгоритм сделать, либо искать официальный
+    every_two_goals_df_gk: ,                                                    #не факт, что это нам надо, можно это в логике сделать(это только для информации если)
+    yellow_cards: row[12],
+    red_cards: row[13],
+    self_goals:                                                                 #надо придумать, откуда взять
+                                                                                #а вообще, давайте попробуем таблицу перфомансов подогнать под данные, которые мы имеем, это имело бы смысл
+                                                                                #
+                                                                                #короче, жду предложения в коментарии
+  )
+  player.perfomance.save
+  end
 end
