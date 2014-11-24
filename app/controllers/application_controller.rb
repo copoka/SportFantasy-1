a41b0d54515d2f4a60407d2edfd96c6fd516b315
AlexanderPower <aleksandr.power88@gmail.com>
2014-11-24 17:26:31 +0500

    Added user.email in header. Variously sing_in/sing_up/sing_out action in header.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
 
   after_filter :set_csrf_cookie_for_ng
 
+  respond_to :html, :json
+
   def set_csrf_cookie_for_ng
     cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
   end
