a933e45f852101719a3a48886df2ddf361595c27
Vlad Alyukov <valyukov@gmail.com>
2015-01-17 22:56:29 +0300

    update rails staff

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
-# Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+Bundler.require(*Rails.groups)
 
 module SportFantasy
   class Application < Rails::Application
@@ -26,5 +21,8 @@ module SportFantasy
     # config.i18n.default_locale = :de
 
     config.autoload_paths += %W(#{config.root}/lib)
+    
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
