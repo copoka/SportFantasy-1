d46a867fbd4423b804384a117efa2cde0a91d38f
AlexanderPower <aleksandr.power88@gmail.com>
2015-01-21 15:31:49 +0500

    Added two checks. Max players count and duplicate.

--- app/models/user_team_player.rb
+++ app/models/user_team_player.rb
@@ -11,22 +11,38 @@
 #
 
 class UserTeamPlayer < ActiveRecord::Base
+  MAX_PLAYERS_COUNT=5
   belongs_to :user_team
   belongs_to :player
 
-  validates  :player_id,
-                presence: true,
-                numericality: { only_integer: true }
+  validates :player_id,
+            presence: true,
+            numericality: {only_integer: true}
 
-  validates  :first_team,
-                :inclusion => {:in => [true, false, nil]}
+  validates :first_team,
+            :inclusion => {:in => [true, false, nil]}
 
   validates :user_team_id,
-                presence: true,
-                numericality: { only_integer: true }
+            presence: true,
+            numericality: {only_integer: true}
 
   before_update do |user_team_player|
-      
+
+  end
+  before_create :check_max_players_count, :check_for_duplicates
+
+  private
+  def check_max_players_count
+    if self.user_team.user_team_players.count>=MAX_PLAYERS_COUNT
+      errors.add(:max_players_count, "in your team is #{MAX_PLAYERS_COUNT}.")
+      return false
+    end
   end
 
+  def check_for_duplicates
+    if self.user_team.players.include? self.player
+      errors.add :player, "#{self.player.name} is already in your team."
+      return false
+    end
+  end
 end
