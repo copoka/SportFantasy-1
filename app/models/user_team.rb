eaa7193e9d969d1482779dadc42ed25b867946ca
AlexanderPower <aleksandr.power88@gmail.com>
2015-01-24 21:12:55 +0500

    Added default positining. Added dinamically football field.

--- app/models/user_team.rb
+++ app/models/user_team.rb
@@ -14,4 +14,18 @@ class UserTeam < ActiveRecord::Base
   belongs_to :user
   has_many :user_team_players
   has_many :players, through: :user_team_players
+
+  POSITIONINGS={
+      '2-4-4' => [
+          ['col-md-6', 'col-md-6'],
+          ['col-md-3', 'col-md-3', 'col-md-3', 'col-md-3'],
+          ['col-md-3', 'col-md-3', 'col-md-3', 'col-md-3']
+      ],
+      '2-5-3' => [
+          ['col-md-6', 'col-md-6'],
+          ['col-md-2 col-offset-1', 'col-md-2', 'col-md-2', 'col-md-2', 'col-md-2'],
+          ['col-md-4', 'col-md-4', 'col-md-4']
+      ]
+  }
+  POSITIONINGS_NAMES= %w(FRD MID DEF)
 end
