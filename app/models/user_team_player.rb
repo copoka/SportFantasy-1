b21babbd4ff191f65c3de7784b11c46ba2b2c277
AlexanderPower <aleksandr.power88@gmail.com>
2015-01-26 17:07:32 +0500

    Added check_place_on_football_field

--- app/models/user_team_player.rb
+++ app/models/user_team_player.rb
@@ -11,7 +11,7 @@
 #
 
 class UserTeamPlayer < ActiveRecord::Base
-  MAX_PLAYERS_COUNT=5
+  MAX_PLAYERS_COUNT=15
   belongs_to :user_team
   belongs_to :player
 
@@ -30,15 +30,23 @@ class UserTeamPlayer < ActiveRecord::Base
 
   end
   before_create :check_max_players_count, :check_for_duplicates
+  before_update :check_place_on_football_field
 
   #names for instance_eval
   FRW_MID_DEF=%w(first_team_forwards first_team_middels first_team_defenders)
-  POSITIONINGS_NAMES= %w(FRW MID DEF)
+  POSITIONINGS_NAMES= %w(FOR MID DEF)
+  AMPLUA_POS_NAMES={
+      'Нападающий' => "FOR",
+      'Полузащитник' => "MID",
+      'Защитник' => "DEF",
+      'Вратарь' => "GK"
+  }
 
   scope :first_team_players, -> { where(first_team: true) }
   scope :first_team_forwards, -> { first_team_players.where(player: Player.forwards) }
   scope :first_team_middels, -> { first_team_players.where(player: Player.middels) }
   scope :first_team_defenders, -> { first_team_players.where(player: Player.defenders) }
+  scope :first_team_goalkeeper, -> { first_team_players.where(player: Player.goalkeepers) }
 
   private
   def check_max_players_count
@@ -51,6 +59,31 @@ class UserTeamPlayer < ActiveRecord::Base
   def check_for_duplicates
     if self.user_team.players.include? self.player
       errors.add :player, "#{self.player.name} is already in your team."
+      false
+    end
+  end
+
+  def check_place_on_football_field
+    return true if self.user_team.positioning.empty?
+    amplua_key= AMPLUA_POS_NAMES[self.player.amplua.name]
+    logger.info self.player.amplua.name.inspect
+    logger.info amplua_key.inspect
+
+    #first team may have only one goalkeeper
+    if amplua_key=='GK'
+      if self.user_team.user_team_players.first_team_goalkeeper.count>=1
+        errors.add('gk_max_count', "in your team is 1.")
+        return false
+      else
+        return true
+      end
+    end
+
+    for_max_count, mid_max_count, def_max_count=self.user_team.positioning.split '-'
+    pos_name=FRW_MID_DEF[POSITIONINGS_NAMES.index(amplua_key)]
+    max_count=eval(amplua_key.downcase+'_max_count')
+    if self.user_team.user_team_players.instance_eval(pos_name).count >= max_count
+      errors.add(amplua_key.downcase+'_max_count', "in your team is #{max_count}.")
       false
     end
   end
